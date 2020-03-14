b72a5ef9ed3133114e67c10ded17dd4e538d19e7
Andor Davoti <andor.davoti@gmail.com>
2020-03-14 17:54:24 +0100

    changed num to be the same as fieldType indexes

--- components/GameCanvas.js
+++ components/GameCanvas.js
@@ -61,7 +61,7 @@ class GameCanvas extends React.Component {
 
         if (fieldType[combination[0]] === user && fieldType[combination[1]] === user && fieldType[combination[2]] === user) {
             if (Platform.OS === 'ios') Haptics.notificationAsync('success')
-            this.setState({ winner: user, disableFields: true, winnerColumns: [combination[0] + 1, combination[1] + 1, combination[2] + 1] })
+            this.setState({ winner: user, disableFields: true, winnerColumns: [combination[0], combination[1], combination[2]] })
         }
     }
 
@@ -75,11 +75,11 @@ class GameCanvas extends React.Component {
         let fieldType = this.state.fieldType,
             turn = this.state.turn
 
-        if (fieldType[num - 1] === '') {
+        if (fieldType[num] === '') {
             if (Platform.OS === 'ios') Haptics.selectionAsync()
 
-            if (turn === 'o') fieldType[num - 1] = 'o'
-            else fieldType[num - 1] = 'x'
+            if (turn === 'o') fieldType[num] = 'o'
+            else fieldType[num] = 'x'
 
             this.checkGame(turn)
 
@@ -98,19 +98,19 @@ class GameCanvas extends React.Component {
             </View>
             <View style={{ flexDirection: 'row' }}>
                 <View>
-                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={1} fieldType={fieldType[0]} />
-                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={4} fieldType={fieldType[3]} />
-                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={7} fieldType={fieldType[6]} />
+                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={0} fieldType={fieldType[0]} />
+                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={3} fieldType={fieldType[3]} />
+                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={6} fieldType={fieldType[6]} />
                 </View>
                 <View>
-                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={2} fieldType={fieldType[1]} />
-                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={5} fieldType={fieldType[4]} />
-                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={8} fieldType={fieldType[7]} />
+                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={1} fieldType={fieldType[1]} />
+                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={4} fieldType={fieldType[4]} />
+                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={7} fieldType={fieldType[7]} />
                 </View>
                 <View>
-                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={3} fieldType={fieldType[2]} />
-                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={6} fieldType={fieldType[5]} />
-                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={9} fieldType={fieldType[8]} />
+                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={2} fieldType={fieldType[2]} />
+                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={5} fieldType={fieldType[5]} />
+                    <Column winnerColumns={winnerColumns} disabled={disableFields} action={this.pressed} num={8} fieldType={fieldType[8]} />
                 </View>
             </View>
         </View>
