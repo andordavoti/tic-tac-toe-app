442c993de31ac195e42b87b644272e9680762687
Andor Davoti <andor.davoti@gmail.com>
2020-04-04 19:12:50 +0200

    images cached on start

--- client/App.js
+++ client/App.js
@@ -1,29 +1,55 @@
-import 'react-native-gesture-handler';
 import * as React from 'react';
 import { View, StatusBar } from 'react-native';
 import { decode, encode } from 'base-64';
 import AppNavigator from './components/AppNavigator';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
 
 // Polyfill for Firebase missing base-64 decoder/encoder
-if (!global.btoa) {
-  global.btoa = encode;
+if (!global.btoa) global.btoa = encode
+if (!global.atob) global.atob = decode
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 }
 
-if (!global.atob) {
-  global.atob = decode;
-}
+export default class App extends React.Component {
+  state = { isReady: false }
 
-const App = () => {
-  return (
-    <Provider store={store}>
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      require('./assets/images/o.png'),
+      require('./assets/images/x.png'),
+      require('./assets/images/clipboard.png'),
+      require('./assets/images/app-store.png'),
+      require('./assets/images/google-play.png'),
+    ]);
+
+    await Promise.all([...imageAssets]);
+  }
+
+
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading
+        startAsync={this._loadAssetsAsync}
+        onFinish={() => this.setState({ isReady: true })}
+        onError={console.warn} />
+    }
+    return <Provider store={store}>
       <View style={{ flex: 1 }}>
         <StatusBar barStyle="light-content" />
         <AppNavigator />
       </View>
     </Provider>
-  );
-};
-
-export default App;
+  }
+}
