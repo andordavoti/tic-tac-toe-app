a1a756e3e10e7ef5819ed67f9716fa8f533ca1b0
Andor Davoti <andor.davoti@gmail.com>
2020-05-14 19:12:52 +0200

    added sentry bug tracker to project

--- client/App.tsx
+++ client/App.tsx
@@ -4,21 +4,37 @@ import { decode, encode } from 'base-64';
 import AppNavigator from './components/AppNavigator';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import * as Sentry from 'sentry-expo';
+import { SENTRY_DSN } from './lib/apiKeys';
+import Constants from 'expo-constants';
 
-declare const global: { HermesInternal: null | {}; btoa: typeof encode; atob: typeof decode };
+declare const global: {
+    HermesInternal: null | {};
+    btoa: typeof encode;
+    atob: typeof decode;
+};
 // Polyfill for Firebase missing base-64 decoder/encoder
 if (!global.btoa) global.btoa = encode;
 if (!global.atob) global.atob = decode;
 
 const App: React.FC = () => {
-  return (
-    <Provider store={store}>
-      <View style={{ flex: 1 }}>
-        <StatusBar barStyle="light-content" />
-        <AppNavigator />
-      </View>
-    </Provider>
-  );
+    Sentry.init({
+        dsn: SENTRY_DSN,
+        enableInExpoDevelopment: false,
+        debug: process.env.NODE_ENV === 'development' ? true : false,
+    });
+
+    if (Constants.manifest.revisionId)
+        Sentry.setRelease(Constants.manifest.revisionId);
+
+    return (
+        <Provider store={store}>
+            <View style={{ flex: 1 }}>
+                <StatusBar barStyle="light-content" />
+                <AppNavigator />
+            </View>
+        </Provider>
+    );
 };
 
 export default App;
