48161bd3544e474262b02b80eb49622a3f19b54c
Andor Davoti <andor.davoti@gmail.com>
2020-05-24 15:13:19 +0200

    removed unused dropdownitems

--- client/lib/dropdownItems.ts
+++ client/lib/dropdownItems.ts
@@ -6,8 +6,8 @@ export const gridSizeDropdownItems = [
     {
         label: '4 x 4',
         value: 4,
-    }
-]
+    },
+];
 
 export const themeDropdownItems = [
     {
@@ -21,48 +21,5 @@ export const themeDropdownItems = [
     {
         label: 'Dark',
         value: 'dark',
-    }
-]
-
-export const maxResultsDropdownItems = [
-    {
-        label: '100 Words',
-        value: 100,
     },
-    {
-        label: '200 Words',
-        value: 200,
-    },
-    {
-        label: '300 Words',
-        value: 300,
-    },
-    {
-        label: '400 Words',
-        value: 400,
-    },
-    {
-        label: '500 Words',
-        value: 500,
-    },
-    {
-        label: '600 Words',
-        value: 600,
-    },
-    {
-        label: '700 Words',
-        value: 700,
-    },
-    {
-        label: '800 Words',
-        value: 800,
-    },
-    {
-        label: '900 Words',
-        value: 900,
-    },
-    {
-        label: '1000 Words',
-        value: 1000,
-    }
-]
+];
