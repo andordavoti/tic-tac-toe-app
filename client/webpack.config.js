3af2d4e66dfc188c3bcabe13a752b1d6a0111ae9
Andor Davoti <andor.davoti@gmail.com>
2020-06-22 11:30:19 +0200

    changed webpack config to use production build of react

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const createExpoWebpackConfig = require('@expo/webpack-config');
 module.exports = function (env, argv) {
-    env.mode = 'development';
+    env.mode = 'production';
     const config = createExpoWebpackConfig(env, argv);
     return config;
 };
