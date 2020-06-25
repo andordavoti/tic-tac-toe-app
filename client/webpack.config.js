539ca9c6705ec7edd5b3adc55d6f431977a309ab
Andor Davoti <andor.davoti@gmail.com>
2020-06-25 12:03:00 +0200

    updated web client webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const createExpoWebpackConfig = require('@expo/webpack-config');
 module.exports = function (env, argv) {
-    env.mode = 'production';
+    env.mode = process.env.NODE_ENV;
     const config = createExpoWebpackConfig(env, argv);
     return config;
 };
