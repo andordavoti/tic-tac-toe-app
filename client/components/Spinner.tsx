ab4f7b7f96d59b3bee9266ccb360857dc0aff046
Andor Davoti <andor.davoti@gmail.com>
2023-05-14 14:48:15 +0200

    refactor styling

--- client/components/Spinner.tsx
+++ client/components/Spinner.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 import { colors, calcFromHeight } from '../lib/constants';
 import { ActivityIndicator } from 'react-native-paper';
-import { ThemeMode } from '../types/Theme';
 import { useDimensions } from '@react-native-community/hooks';
 import { useSelectedTheme } from '../redux/settingsSlice';
 
@@ -13,29 +12,23 @@ interface Props {
 
 const Spinner: React.FC<Props> = ({ msg, size = 'large' }) => {
     const theme = useSelectedTheme();
-
     const { height } = useDimensions().window;
-
-    const styles = getStyleSheet(theme, height);
-
     return (
         <View style={{ marginTop: calcFromHeight(10, height) }}>
             <ActivityIndicator color={colors[theme].main} size={size} />
-            <Text style={styles.text}>{msg}</Text>
+            <Text
+                style={{
+                    color: colors[theme].text,
+                    fontWeight: 'bold',
+                    fontSize: 15,
+                    margin: calcFromHeight(20, height),
+                    textAlign: 'center',
+                }}
+            >
+                {msg}
+            </Text>
         </View>
     );
-};
-
-const getStyleSheet = (theme: ThemeMode, height: number) => {
-    return StyleSheet.create({
-        text: {
-            color: colors[theme].text,
-            fontWeight: 'bold',
-            fontSize: 15,
-            margin: calcFromHeight(20, height),
-            textAlign: 'center',
-        },
-    });
 };
 
 export default Spinner;
