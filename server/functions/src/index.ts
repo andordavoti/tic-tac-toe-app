58102866dc100c64d682290c60d44b8c519244e6
Andor Davoti <andor.davoti@gmail.com>
2023-05-14 22:06:26 +0200

    upgrade runtime to node 16, fix garbage collector

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,5 +1,7 @@
-import * as functions from 'firebase-functions';
 import * as firebaseAdmin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import { logger } from 'firebase-functions';
+import dayjs from 'dayjs';
 import app from './server';
 
 const admin = firebaseAdmin.initializeApp();
@@ -8,20 +10,29 @@ const firestore = admin.firestore();
 
 exports.game = functions.https.onRequest(app(firestore));
 
-exports.garbageCollector = functions.pubsub
+export const garbageCollector = functions.pubsub
     .schedule('every 48 hours')
-    .onRun(async context => {
-        const today = Date.now();
-        const fourtyEightHoursAgo = new Date(today - 86400000);
+    .onRun(async () => {
+        const fourtyEightHoursAgo = firebaseAdmin.firestore.Timestamp.fromDate(
+            dayjs().subtract(2, 'days').toDate()
+        );
+
         const garbageCollections = firestore
             .collection('lobbies')
             .where('createdAt', '<', fourtyEightHoursAgo);
 
         const querySnapshot = await garbageCollections.get();
-        if (querySnapshot.empty) return;
+        if (querySnapshot.empty) {
+            logger.log('Garbage Collector Exited - Nothing to delete');
+            return;
+        }
 
         const batch = firestore.batch();
         querySnapshot.docs.forEach(doc => batch.delete(doc.ref));
 
         await batch.commit();
+
+        logger.log(
+            `Garbage Collector Finished - Deleted ${querySnapshot.size} documents`
+        );
     });
