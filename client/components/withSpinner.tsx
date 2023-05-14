df7777e07989d6be0eefd1c9f7ba43e289ce22f7
Andor Davoti <andor.davoti@gmail.com>
2023-05-14 14:48:22 +0200

    prettier format

--- client/components/withSpinner.tsx
+++ client/components/withSpinner.tsx
@@ -6,9 +6,15 @@ interface PropTypes {
     msg: string;
 }
 
-const withSpinner = <P extends object>(
-    WrappedComponent: React.ComponentType<P>
-): React.FC<P & PropTypes> => ({ loading = true, msg, ...props }: PropTypes) =>
-    loading ? <Spinner msg={msg} /> : <WrappedComponent {...(props as P)} />;
+const withSpinner =
+    <P extends object>(
+        WrappedComponent: React.ComponentType<P>
+    ): React.FC<P & PropTypes> =>
+    ({ loading = true, msg, ...props }: PropTypes) =>
+        loading ? (
+            <Spinner msg={msg} />
+        ) : (
+            <WrappedComponent {...(props as P)} />
+        );
 
 export default withSpinner;
